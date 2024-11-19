ad745387135275a6b3730e568f5a0c668f46ff36
v0lp3 <andreamaujeri@gmail.com>
2024-11-19 19:05:01 +0100

    Refactor

--- tuner.py
+++ tuner.py
@@ -4,41 +4,10 @@ import requests
 import re
 import json
 import base64
+
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from urllib.parse import urlparse
 
-# Set variable "cache" as the environment variable or default value
-cache = os.getenv("CACHE")
-assert cache is not None, "CACHE environment variable is not set"
-cache = base64.b64decode(cache).decode().strip().split("|")
-
-# Set variable "json" as the environment variable or default value
-json_data = os.getenv("JSON")
-assert json_data is not None, "JSON environment variable is not set"
-
-json_data = base64.b64decode(json_data).decode().strip()
-json_data = json.loads(json_data)
-
-lookup_service = json_data.get("lookup_service")
-
-# Fetch the list of TLDs
-tlds_response = requests.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
-assert tlds_response.status_code == 200, "Failed to fetch TLDs"
-
-tlds = tlds_response.text.splitlines()[1:]  # Skip the first line
-
-
-def check_domain_cache(domain, tld, validator):
-    try:
-        response = requests.get(
-            f"https://{domain}.{tld}", timeout=5, allow_redirects=True
-        )
-        if re.search(re.escape(validator), response.text):
-            tld = urlparse(response.url).hostname.split(".")[-1].upper()
-            return tld
-    except requests.exceptions.RequestException:
-        pass
-    return None
+domaint_fmt = "https://{domain}.{tld}"
 
 
 def validate_domain(url, validator):
@@ -77,54 +46,46 @@ def extract_from_lookup(domain) -> set[str]:
     return set()
 
 
+def verify_tlds(domain, tlds, validator, scope, site_id):
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        futures = []
+
+        for tld in tlds:
+            test_domain = domaint_fmt.format(domain=domain, tld=tld)
+            futures += [executor.submit(validate_domain, test_domain, validator)]
+
+            for future in as_completed(futures):
+                if future.result() == True:
+                    print(f"Site #{site_id+1} found in {scope}", file=sys.stderr)
+                    executor.shutdown(wait=False, cancel_futures=True)
+                    return tld
+
+        print(f"Site #{site_id+1} not found in {scope}", file=sys.stderr)
+        return None
+
+
 def find_domain(i, site, cache_tld):
     domain = site["domain"]
     validator = site["validator"]
 
-    # Helper function to check each TLD in parallel
-    def check_tld(tld):
-        full_url = f"https://{domain}.{tld}"
-        if validate_domain(full_url, validator):
-            return tld
-        return None
-
     # Check the cache first
-    if cache_tld:
-        tld = check_domain_cache(domain, cache_tld, validator)
+    if cache_tld and (tld := verify_tlds(domain, [cache_tld], validator, "cache", i)):
+        return tld
 
-        if tld:
-            print(f"Site #{i+1} found in cache", file=sys.stderr)
-            return tld
-        else:
-            print(f"Site #{i+1} not found in cache", file=sys.stderr)
+    if (fast_path := site.get("fast_path")) and (
+        tld := verify_tlds(
+            domain, extract_from_fastpath(fast_path, domain), validator, "fastpath", i
+        )
+    ):
+        return tld
 
-    if fast_path := site.get("fast_path"):
-        for tld in extract_from_fastpath(fast_path, domain):
-            if validate_domain(f"https://{domain}.{tld}", validator):
-                print(f"Site #{i+1} found in fastpath", file=sys.stderr)
-                return tld
+    if lookup_service and (
+        tld := verify_tlds(domain, extract_from_lookup(domain), validator, "lookup", i)
+    ):
+        return tld
 
-    print(f"Site #{i+1} not found in fastpath", file=sys.stderr)
-
-    for tld in extract_from_lookup(domain):
-        if validate_domain(f"https://{domain}.{tld}", validator):
-            print(f"Site #{i+1} found in lookup", file=sys.stderr)
-            return tld
-
-    print(f"Site #{i+1} not found in lookup", file=sys.stderr)
-
-    # Run the TLD checks in parallel
-    with ThreadPoolExecutor(max_workers=100) as executor:
-
-        futures = [executor.submit(check_tld, tld) for tld in tlds]
-
-        # Wait for the first successful result
-        for future in as_completed(futures):
-            tld = future.result()
-            if tld:
-                executor.shutdown(wait=False, cancel_futures=True)
-                return tld
-        return None
+    if tld := verify_tlds(domain, tlds, validator, "all tlds", i):
+        return tld
 
 
 def find_domains(sites, cache):
@@ -142,22 +103,44 @@ def find_domains(sites, cache):
     return "|".join(domains)
 
 
-def main():
-    global cache
-
-    sites = json_data["sites"]
-
-    if len(cache) != len(sites):
-        print(
-            f"Cache length does not match sites length. Cache len:{len(cache)} != Sites len:{len(sites)}. Skipping it",
-            file=sys.stderr,
-        )
-        cache = [None] * len(sites)
-
-    out = find_domains(sites, cache)
-
-    encoded_out = base64.b64encode(out.encode()).decode()
-    print(encoded_out)
+def parse_cache():
+    cache = os.getenv("CACHE")
+    assert cache is not None, "CACHE environment variable is not set"
+    return base64.b64decode(cache).decode().strip().split("|")
 
 
-main()
+def parse_config():
+    config_str = os.getenv("CONFIG")
+    assert config_str is not None, "JSON environment variable is not set"
+
+    config_str = base64.b64decode(config_str).decode().strip()
+    return json.loads(config_str)
+
+
+def cache_all_tlds():
+    # Fetch the list of TLDs
+    tlds_response = requests.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+    assert tlds_response.status_code == 200, "Failed to fetch TLDs"
+
+    return tlds_response.text.splitlines()[1:]  # Skip the first line
+
+
+cache = parse_cache()
+config = parse_config()
+tlds = cache_all_tlds()
+
+lookup_service = config.get("lookup_service")
+sites = config["sites"]
+
+if len(cache) != len(sites):
+    print(
+        f"Cache length does not match sites length. Cache len:{len(cache)} != Sites len:{len(sites)}. Skipping it",
+        file=sys.stderr,
+    )
+    cache = [None] * len(sites)
+
+out = find_domains(sites, cache)
+
+encoded_out = base64.b64encode(out.encode()).decode()
+
+print(encoded_out)
